7408728b39ffc4dab05a42aed09aae403408e8ab
Molebatsi <batsi.seroka@gmail.com>
2024-09-30 13:02:44 +0200

    python project

---
+++ .venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\Users\Batsi\OneDrive\Documents\AppDev\Python Project\channels-examples\.venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
